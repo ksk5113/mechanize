5c6208797a5da4e0f1e2b79785b492e64a70eab8
ksk5113 <kskksk5113@gmail.com>
2016-08-17 07:09:54 +0000

    mechanize

---
+++ app/models/lecture.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'mechanize'
+
+class Lecture < ActiveRecord::Base
+	def self.crawling
+		my_page = login
+		a = my_page.link_with(:href => '/lectures').click
+		result_crawling = a.search('.title').map(&:text)
+		result_crawling.each do |i|
+			Lecture.create(title: i)
+		end
+	end
+
+	def self.login
+		agent = Mechanize.new
+		page = agent.get('http://class.likelion.net/users/sign_in')
+		my_page = page.form_with(:action => '/users/sign_in') do |f|
+			f.field_with(:name => 'user[email]').value = 'kskksk5113@gmail.com'
+			f.field_with(:name => 'user[password]').value = 'brit5113'
+		end.submit
+	    
+        return my_page
+    end
+    
+end
